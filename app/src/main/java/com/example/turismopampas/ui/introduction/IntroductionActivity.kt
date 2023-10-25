c2e10139a03bbc1f3c01497609ed0df44cc4822d
Denil Ramos <84945046+DenilRamos@users.noreply.github.com>
2023-10-25 17:11:53 -0500

    disenio del interface

---
+++ app/src/main/java/com/example/turismopampas/ui/introduction/IntroductionActivity.kt
@@ -0,0 +1,11 @@
+package com.example.turismopampas.ui.introduction
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.turismopampas.R
+class IntroductionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_introduction)
+    }
+}
